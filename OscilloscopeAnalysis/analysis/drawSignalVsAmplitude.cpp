6134494ac0026adea7216e4609d24038caaa441d
Francesco Pandolfi <pandolf@pMacbookINFN.local>
2019-05-03 11:24:57 +0200

    added ampMax

--- OscilloscopeAnalysis/analysis/drawSignalVsAmplitude.cpp
+++ OscilloscopeAnalysis/analysis/drawSignalVsAmplitude.cpp
@@ -8,14 +8,19 @@
 #include "TGraphErrors.h"
 #include "TH2D.h"
 #include "TLine.h"
+#include "TLegend.h"
 
 #include "NanoUVCommon.h"
+
+
+bool use_ampMax = true;
 
 
 float getSignalFromFile( const std::string& fileName );
 void removeTimeOffset( TGraphErrors* graph );
 float getPedestal( TGraphErrors* graph, int nPoints );
 float integrateSignal( TGraphErrors* graph, float pedestal );
+float ampMaxSignal( TGraphErrors* graph, float pedestal );
 
 
 
@@ -51,11 +56,13 @@ int main( int argc, char* argv[] ) {
 
   int nFiles = 50;
 
-  float calibrationConst = 1000.;
+  float calibrationConst = (use_ampMax) ? 1. : 1000.;
 
   TFile* file_signal = TFile::Open( Form("signalHistos_%s.root", prodName.c_str()), "recreate" );
 
   TGraphErrors* gr_signal_vs_ampl = new TGraphErrors(0);
+  TGraphErrors* gr_signal_vs_ampl_sigmaUp = new TGraphErrors(0);
+  TGraphErrors* gr_signal_vs_ampl_sigmaDn = new TGraphErrors(0);
 
   for( unsigned i=0; i<amplitudes.size(); ++i ) {
 
@@ -75,9 +82,10 @@ int main( int argc, char* argv[] ) {
     float xValue = amplitudes[i]*300.; // number of photons (see LED driver data sheet)
 
     int iPoint = gr_signal_vs_ampl->GetN();
-    gr_signal_vs_ampl->SetPoint     ( iPoint, xValue, h1_signal->GetMean() );
-    //gr_signal_vs_ampl->SetPointError( iPoint,            0., h1_signal->GetRMS () );
-    gr_signal_vs_ampl->SetPointError( iPoint,            0., h1_signal->GetMeanError() );
+    gr_signal_vs_ampl        ->SetPoint( iPoint, xValue, h1_signal->GetMean() );
+    gr_signal_vs_ampl_sigmaUp->SetPoint( iPoint, xValue, h1_signal->GetMean()+h1_signal->GetRMS() );
+    gr_signal_vs_ampl_sigmaDn->SetPoint( iPoint, xValue, h1_signal->GetMean()-h1_signal->GetRMS() );
+    gr_signal_vs_ampl->SetPointError( iPoint, 0., h1_signal->GetMeanError() );
 
   } // for amplitudes
 
@@ -90,22 +98,41 @@ int main( int argc, char* argv[] ) {
   c1->cd();
 
   float xMin = 0.;
-  float xMax = 11.*300;
-  float yMin = -5000./calibrationConst;
-  float yMax = 500./calibrationConst;
+  float xMax = 10.*300;
+  float yMin = (use_ampMax) ? -180. : -5.;
+  float yMax = (use_ampMax) ? 0. : 0.5;
 
   TH2D* h2_axes = new TH2D("axes", "", 10, xMin, xMax, 10, yMin, yMax );
-  h2_axes->SetYTitle("Integrated Signal [a.u.]"); 
+  if( use_ampMax ) 
+    h2_axes->SetYTitle("AmpMax Signal (Gain = 150) [a.u.]"); 
+  else
+    h2_axes->SetYTitle("Integrated Signal (Gain = 150) [a.u.]"); 
   h2_axes->SetXTitle("Number of Photons");
   h2_axes->Draw();
 
-  TLine* line_zero = new TLine( xMin, 0., xMax, 0. );
+
+  double x0, y0;
+  gr_signal_vs_ampl->GetPoint( gr_signal_vs_ampl->GetN()-1, x0, y0 );
+  float lineY = (use_ampMax) ? y0 : 0.;
+
+  TLine* line_zero = new TLine( xMin, lineY, xMax, lineY );
   line_zero->Draw("same");
+
+  gr_signal_vs_ampl_sigmaUp->SetLineColor(38);
+  gr_signal_vs_ampl_sigmaUp->SetLineWidth(2);
+  gr_signal_vs_ampl_sigmaUp->SetLineStyle(2);
+  gr_signal_vs_ampl_sigmaUp->Draw("L same");
+
+  gr_signal_vs_ampl_sigmaDn->SetLineColor(38);
+  gr_signal_vs_ampl_sigmaDn->SetLineWidth(2);
+  gr_signal_vs_ampl_sigmaDn->SetLineStyle(2);
+  gr_signal_vs_ampl_sigmaDn->Draw("L same");
 
   gr_signal_vs_ampl->SetMarkerStyle(20);
   gr_signal_vs_ampl->SetMarkerSize(1.6);
   gr_signal_vs_ampl->SetMarkerColor(46);
   gr_signal_vs_ampl->Draw("PL same");
+
 
   TPaveText* label_led = new TPaveText( 0.23, 0.55, 0.6, 0.62, "brNDC" );
   label_led->SetFillColor(0);
@@ -123,6 +150,13 @@ int main( int argc, char* argv[] ) {
   label_gamma->AddText("380 < #lambda < 420 nm");
   label_gamma->AddText("2.9 < E_{#gamma} < 3.3 eV");
   label_gamma->Draw("same");
+
+  TLegend* legend = new TLegend( 0.6, 0.3, 0.88, 0.4 );
+  legend->SetFillColor(0);
+  legend->SetTextSize(0.035);
+  legend->SetTextFont(42);
+  legend->AddEntry( gr_signal_vs_ampl_sigmaDn, "68% band", "L" );
+  legend->Draw("same");
 
   NanoUVCommon::addNanoUVLabel(c1, 3);
 
@@ -191,7 +225,7 @@ float getSignalFromFile( const std::string& fileName ) {
   graph->GetPoint( graph->GetN()-1, xmax, ymax );
 
 
-  TH2D* h2_axes = new TH2D( Form("axes_%s", fileName.c_str()), "", 10, x0, xmax, 10, pedestal-100., pedestal+10. );
+  TH2D* h2_axes = new TH2D( Form("axes_%s", fileName.c_str()), "", 10, x0, xmax, 10, pedestal-150., pedestal+15. );
   h2_axes->SetXTitle( "Time [ns]" );
   h2_axes->SetYTitle( "Amplitude [mV]" );
   h2_axes->Draw();
@@ -208,7 +242,8 @@ float getSignalFromFile( const std::string& fileName ) {
 
   c1->SaveAs( Form("%s.pdf", fileName.c_str()) );
   
-  float signal = integrateSignal( graph, pedestal );
+  float signal = ampMaxSignal( graph, pedestal );
+  //float signal = integrateSignal( graph, pedestal );
 
   delete graph;
   delete c1;
@@ -268,6 +303,25 @@ float integrateSignal( TGraphErrors* graph, float pedestal ) {
   }
 
   return signal;
+
+}
+
+
+float ampMaxSignal( TGraphErrors* graph, float pedestal ) {
+
+  float ampMax = 0.;
+
+  for( unsigned iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y );
+
+    float thisPoint = (y-pedestal);
+    if( thisPoint < ampMax ) ampMax = thisPoint;
+
+  }
+
+  return ampMax;
 
 }
 
