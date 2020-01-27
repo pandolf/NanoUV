46683776f2b888577db397131064f829f363da5a
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2020-01-27 22:00:31 +0100

    latest improvements, including saving with hvAPD in name

--- ElectronGunAnalysis/analysis/drawAPDGunScan.cpp
+++ ElectronGunAnalysis/analysis/drawAPDGunScan.cpp
@@ -15,7 +15,8 @@
 
 
 
-std::string data = "2019_11_28";
+std::string data = "2020_01";
+//std::string data = "2019_11_28";
 
 
 int main( int argc, char* argv[] ) {
@@ -47,40 +48,79 @@ int main( int argc, char* argv[] ) {
   float xMax;
   float yMax;
 
-  if( gunEnergy == 500. ) {
+  if( data == "2019_11_28" ) {
 
-    gst.addPointToGraph( gr_iapd_vs_igun, "Ek_500_750fA_dfh_APD__28nov19_07_M_.dat", 0.8, 0.75 );
-    gst.addPointToGraph( gr_iapd_vs_igun, "Ek_500_3.6pA_dfh_APD__28nov19_06_M_.dat", 3.7, 3.6  );
-    gst.addPointToGraph( gr_iapd_vs_igun, "Ek_500_26pA_dfh_APD__28nov19_05_M_.dat" , 27.7, 26. );
+    if( gunEnergy == 500. ) {
+  
+      gst.addPointToGraph( gr_iapd_vs_igun, "Ek_500_750fA_dfh_APD__28nov19_07_M_.dat", 0.8, 0.75 );
+      gst.addPointToGraph( gr_iapd_vs_igun, "Ek_500_3.6pA_dfh_APD__28nov19_06_M_.dat", 3.7, 3.6  );
+      gst.addPointToGraph( gr_iapd_vs_igun, "Ek_500_26pA_dfh_APD__28nov19_05_M_.dat" , 27.7, 26. );
+  
+      xMax = 30.;
+      yMax = 13000.;
+  
+    } else if( gunEnergy == 900. ) {
+  
+      gst.addPointToGraph( gr_iapd_vs_igun, "Ek_900_450fA_dfv.txt", 0.44, 0.45 );
+      gst.addPointToGraph( gr_iapd_vs_igun, "Ek_900_3.3pA_dfv_28nov19_01_M_.dat", 1.5, 3.3 );
+      gst.addPointToGraph( gr_iapd_vs_igun, "Ek_900_15pA_dfv.txt", 15.5, 17. );
+  
+      xMax = 20.;
+      yMax = 75000.;
+  
+    }
 
-//  gr_iapd_vs_igun->SetPoint     ( gr_iapd_vs_igun->GetN()  , 0.5*(0.8+0.75), 1000.*get_iAPD("Ek_500_750fA_dfh_APD__28nov19_07_M_.dat", 500, 0.8 /*0.75*/) );
-//  gr_iapd_vs_igun->SetPointError( gr_iapd_vs_igun->GetN()-1, 0.05, 10. );
+  } else if( data == "2020_01" ) {
 
-//  gr_iapd_vs_igun->SetPoint     ( gr_iapd_vs_igun->GetN(), 0.5*(3.7+3.6) , 1000.*get_iAPD("Ek_500_3.6pA_dfh_APD__28nov19_06_M_.dat", 500, 3.7 /*3.6*/) );
-//  gr_iapd_vs_igun->SetPointError( gr_iapd_vs_igun->GetN()-1, 0.1, 10. );
+    gst.set_currentMethod("firstMax");
+    gst.set_lastN_fit(0);
 
-//  gr_iapd_vs_igun->SetPoint     ( gr_iapd_vs_igun->GetN(), 0.5*(27.7+26.) , 1000.*get_iAPD("Ek_500_26pA_dfh_APD__28nov19_05_M_.dat" , 500, 27.7 /*26.*/) );
-//  gr_iapd_vs_igun->SetPointError( gr_iapd_vs_igun->GetN()-1, 1.7, 10. );
+    if( gunEnergy == 900. ) {
+  
+      if( APDhv == 380. ) {
 
-    xMax = 30.;
-    yMax = 13000.;
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_05_M_.dat", 1.21, 1.21 ); // see logbook
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_09_M_.dat", 0.378, 0.375 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_13_M_.dat", 0.106, 0.111 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_17_M_.dat", 0.039, 0.040 );
+        //gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_01_M_.dat", 165., 207. );
+        gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_18_M_.dat", 9.9, 10.2 );
 
-  } else if( gunEnergy == 900. ) {
+      } else if( APDhv == 370. ) {
 
-//  gr_iapd_vs_igun->SetPoint     ( gr_iapd_vs_igun->GetN(), 0.5*(0.44+0.45), 1000.*get_iAPD("Ek_900_450fA_dfv.txt", 900, 0.44) );
-//  gr_iapd_vs_igun->SetPointError( gr_iapd_vs_igun->GetN()-1, 0.01, 1000. );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_06_M_.dat", 1.21, 1.21 ); 
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_10_M_.dat", 0.378, 0.375 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_14_M_.dat", 0.106, 0.111 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_18_M_.dat", 0.039, 0.040 );
+        //gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_03_M_.dat", 165., 207. );
+        gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_19_M_.dat", 9.9, 10.2 );
+  
+      } else if( APDhv == 360. ) {
 
-//  gr_iapd_vs_igun->SetPoint     ( gr_iapd_vs_igun->GetN(), 0.5*(1.5+3.3) , 1000.*get_iAPD("Ek_900_3.3pA_dfv_28nov19_01_M_.dat", 900, 1.5) );
-//  gr_iapd_vs_igun->SetPointError( gr_iapd_vs_igun->GetN()-1, 1.8, 10. );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_07_M_.dat", 1.21, 1.21 ); 
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_11_M_.dat", 0.378, 0.375 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_15_M_.dat", 0.106, 0.111 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_19_M_.dat", 0.039, 0.040 );
+        //gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_04_M_.dat", 165., 207. );
+        gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_20_M_.dat", 9.9, 10.2 );
+  
+      } else if( APDhv == 350. ) {
 
-//  gr_iapd_vs_igun->SetPoint( gr_iapd_vs_igun->GetN(), 0.5*(15.5+17.) , 1000.*get_iAPD("Ek_900_15pA_dfv.txt" , 900, 17) );
-//  gr_iapd_vs_igun->SetPointError( gr_iapd_vs_igun->GetN()-1, 1.5, 1000. );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_08_M_.dat", 1.21, 1.21 ); 
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_12_M_.dat", 0.378, 0.375 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_16_M_.dat", 0.106, 0.111 );
+        gst.addPointToGraph( gr_iapd_vs_igun, "22gen20_20_M_.dat", 0.039, 0.040 );
+        //gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_05_M_.dat", 165., 207. );
+        gst.addPointToGraph( gr_iapd_vs_igun, "23gen20_21_M_.dat", 9.9, 10.2 );
+  
+      } // APDhv
 
-    xMax = 20.;
-    yMax = 75000.;
+      xMax = 20.;
+      yMax = 11000.;
 
-  }
+    } // if gunEnergy
 
+  } // data
 
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
   c1->cd();
@@ -90,6 +130,19 @@ int main( int argc, char* argv[] ) {
   h2_axes->SetXTitle( "Gun Current [pA]" );
   h2_axes->SetYTitle( "APD Current [pA]" );
   h2_axes->Draw();
+
+  TPaveText* label_settings = new TPaveText( 0.23, 0.65, 0.5, 0.77, "brNDC" );
+  label_settings->SetTextSize( 0.035 );
+  label_settings->SetFillColor(0);
+  label_settings->AddText( Form("E_{gun} = %.0f eV", gunEnergy) );
+  label_settings->AddText( Form("I_{APD} = %.0f V", gst.APDhv()) );
+  label_settings->SetTextAlign(11);
+  label_settings->Draw("same");
+
+  TPaveText* label = NanoUVCommon::getNanoUVLabel(2);
+  label->Draw("same");  
+  gPad->RedrawAxis();
+
 
   TF1* f1_line = new TF1( "lineScan", "[0] + [1]*x", 0., xMax );
   gr_iapd_vs_igun->Fit( f1_line, "QR0" );
@@ -115,20 +168,8 @@ int main( int argc, char* argv[] ) {
   fitResults->AddText( Form("m = %.1f #pm %.1f", f1_line->GetParameter(1), f1_line->GetParError(1) ) );
   fitResults->Draw("same");
 
-  TPaveText* label_settings = new TPaveText( 0.23, 0.6, 0.5, 0.72, "brNDC" );
-  label_settings->SetTextSize( 0.035 );
-  label_settings->SetFillColor(0);
-  label_settings->AddText( Form("E_{gun} = %.0f eV", gunEnergy) );
-  label_settings->AddText( "I_{APD} = 380 V" );
-  label_settings->SetTextAlign(11);
-  label_settings->Draw("same");
-
-  TPaveText* label = NanoUVCommon::getNanoUVLabel(2);
-  label->Draw("same");  
-  gPad->RedrawAxis();
-
-  c1->SaveAs( Form("plots/APDscans/%s/iapd_vs_igun_E%.0f.pdf", data.c_str(), gunEnergy) );
-  c1->SaveAs( Form("plots/APDscans/%s/iapd_vs_igun_E%.0f.eps", data.c_str(), gunEnergy) );
+  c1->SaveAs( Form("plots/APDscans/%s/iapd_vs_igun_E%.0f_hv%.0f.pdf", data.c_str(), gunEnergy, gst.APDhv()) );
+  c1->SaveAs( Form("plots/APDscans/%s/iapd_vs_igun_E%.0f_hv%.0f.eps", data.c_str(), gunEnergy, gst.APDhv()) );
   
   return 0;
 
