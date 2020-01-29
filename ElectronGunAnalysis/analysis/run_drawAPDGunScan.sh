ae55385e0116e86ce74fb5446f61d07125b12f56
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2020-01-29 15:39:31 +0100

    first commit

---
+++ ElectronGunAnalysis/analysis/run_drawAPDGunScan.sh
@@ -0,0 +1,9 @@
+./drawAPDGunScan 900 350;
+./drawAPDGunScan 900 360;
+./drawAPDGunScan 900 370;
+./drawAPDGunScan 900 380;
+
+#./drawAPDGunScan 500 350;
+#./drawAPDGunScan 500 360;
+#./drawAPDGunScan 500 370;
+#./drawAPDGunScan 500 380;
