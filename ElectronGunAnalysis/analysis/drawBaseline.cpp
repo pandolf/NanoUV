cebec93305ad725f92d593436bab1875f70d3f82
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2020-02-07 15:37:05 +0100

    works

--- ElectronGunAnalysis/analysis/drawBaseline.cpp
+++ ElectronGunAnalysis/analysis/drawBaseline.cpp
@@ -19,6 +19,7 @@ std::string data = "2020_01";
 //std::string data = "2019_11_28";
 
 void addBaselineMeanPoint( TGraphErrors* gr_baseline_vs_APDhv, float APDhv, TGraphErrors* gr_baseline_vs_igun );
+TGraphErrors* normalizeToFirstPoint( TGraphErrors* graph );
 
 
 
@@ -37,6 +38,7 @@ int main( int argc, char* argv[] ) {
   std::vector< TGraphErrors* > graphs;
   TGraphErrors* gr_baseline_vs_APDhv = new TGraphErrors(0);
 
+  float x0 = -6.; // point at which the baseline will be evaluated (should be at APD center)
 
 
   for( float APDhv=350.; APDhv <= 380.; APDhv += 10. ) {
@@ -47,7 +49,7 @@ int main( int argc, char* argv[] ) {
     gr_baseline_vs_igun->SetName( Form( "V_{APD} = %.0f V", APDhv) );
 
     for( unsigned iScan=0; iScan<scans.size(); ++iScan )
-      gr_baseline_vs_igun->SetPoint( gr_baseline_vs_igun->GetN(), scans[iScan]->gunCurrent(), scans[iScan]->getBaseline(-6.) );
+      gr_baseline_vs_igun->SetPoint( gr_baseline_vs_igun->GetN(), scans[iScan]->gunCurrent(), scans[iScan]->getBaseline(x0) );
 
     gr_baseline_vs_igun->SetMarkerStyle( 20 ); 
     gr_baseline_vs_igun->SetMarkerSize( 1.4 ); 
@@ -98,6 +100,92 @@ int main( int argc, char* argv[] ) {
 
   c1->Clear();
 
+
+  std::vector< std::pair< float, float > > gunCurrents;
+  gunCurrents.push_back( std::pair< float, float > (0.03, 0.04) );
+  gunCurrents.push_back( std::pair< float, float > (0.1 ,  0.2) );
+  gunCurrents.push_back( std::pair< float, float > (1.  ,   2.) );
+  gunCurrents.push_back( std::pair< float, float > (9.  ,  10.) );
+  gunCurrents.push_back( std::pair< float, float > (29. ,  30.) );
+  gunCurrents.push_back( std::pair< float, float > (100., 200.) );
+
+  std::vector< GunScan* > scans  = GunScan::loadScans( Form("data/%s/scans.dat", data.c_str()), gunEnergy);
+
+  for( unsigned iCurrent=0; iCurrent<gunCurrents.size(); ++iCurrent ) {
+
+    TGraphErrors* gr_iapd_vs_APDhv = new TGraphErrors(0);
+    TGraphErrors* gr_baseline_vs_APDhv = new TGraphErrors(0);
+
+    gr_iapd_vs_APDhv    ->SetName( Form("gr_iapd_vs_APDhv_i%d"    , iCurrent) );
+    gr_baseline_vs_APDhv->SetName( Form("gr_baseline_vs_APDhv_i%d", iCurrent) );
+
+    std::string gunCurrentText;
+
+    for( unsigned iScan=0; iScan<scans.size(); ++iScan ) {
+
+      if( scans[iScan]->gunCurrent() < gunCurrents[iCurrent].first  ) continue;
+      if( scans[iScan]->gunCurrent() > gunCurrents[iCurrent].second ) continue;
+
+      gunCurrentText = scans[iScan]->gunCurrentText();
+
+      gr_iapd_vs_APDhv    ->SetPoint( gr_iapd_vs_APDhv    ->GetN(), scans[iScan]->APDhv(), scans[iScan]->getCurrentFromScan() );
+      gr_baseline_vs_APDhv->SetPoint( gr_baseline_vs_APDhv->GetN(), scans[iScan]->APDhv(), scans[iScan]->getBaseline(x0) );
+
+    } // for scans
+
+    TGraphErrors* gr_iapd_vs_APDhv_norm     = normalizeToFirstPoint( gr_iapd_vs_APDhv );
+    TGraphErrors* gr_baseline_vs_APDhv_norm = normalizeToFirstPoint( gr_baseline_vs_APDhv );
+
+    TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
+    c1->cd();
+
+    float xMin = 341.;
+    float xMax = 389.;
+
+    TH2D* h2_axes = new TH2D( "axes", "", 10, xMin, xMax, 10, 0., 10 );
+    h2_axes->SetXTitle( "V_{APD} [V]" );
+    h2_axes->SetYTitle( "Relative Current Intensity" );
+    h2_axes->Draw();
+
+    TLegend* legend = new TLegend( 0.2, 0.6, 0.5, 0.8 );
+    legend->SetFillColor(0);
+    legend->SetTextSize(0.035);
+    legend->AddEntry( gr_iapd_vs_APDhv_norm, "I_{APD}", "LP" );
+    legend->AddEntry( gr_baseline_vs_APDhv_norm, "Baseline", "LP" );
+    legend->Draw("same");
+
+    gr_baseline_vs_APDhv_norm->SetMarkerStyle(24);
+    gr_baseline_vs_APDhv_norm->SetMarkerSize(1.3);
+    gr_baseline_vs_APDhv_norm->SetMarkerColor(kGray+3);
+    gr_baseline_vs_APDhv_norm->SetLineColor(kGray+3);
+    gr_baseline_vs_APDhv_norm->Draw("PL same");
+
+    gr_iapd_vs_APDhv_norm->SetMarkerStyle(20);
+    gr_iapd_vs_APDhv_norm->SetMarkerSize(1.3);
+    gr_iapd_vs_APDhv_norm->SetMarkerColor(kGray+3);
+    gr_iapd_vs_APDhv_norm->SetLineColor(kGray+3);
+    gr_iapd_vs_APDhv_norm->Draw("PL same");
+
+    TPaveText* label_gun = new TPaveText( 0.7, 0.2, 0.9, 0.3, "brNDC" );
+    label_gun->SetFillColor(0);
+    label_gun->SetTextSize(0.035);
+    label_gun->AddText( Form("%s", gunCurrentText.c_str()) );
+    label_gun->Draw("same");
+
+    NanoUVCommon::addNanoUVLabel( c1, 2 );
+
+    c1->SaveAs( Form("%s/ratios_vs_APDhv_i%d.eps", outdir.c_str(), iCurrent) );
+    c1->SaveAs( Form("%s/ratios_vs_APDhv_i%d.pdf", outdir.c_str(), iCurrent) );
+
+    delete c1;
+    delete h2_axes;
+    delete legend;
+    //delete line_one;
+
+  } // for iCurrent
+
+
+
   //c1->SetLogx();
   //c1->SetLogy();
 
@@ -146,5 +234,41 @@ void addBaselineMeanPoint( TGraphErrors* gr_baseline_vs_APDhv, float APDhv, TGra
 
   gr_baseline_vs_APDhv->SetPoint     ( thisPoint, APDhv, meanBaseline );
   gr_baseline_vs_APDhv->SetPointError( thisPoint,    0.,  rmsBaseline );
+
+}
+
+
+
+TGraphErrors* normalizeToFirstPoint( TGraphErrors* graph ) {
+
+  double x0=999999., y0;
+
+  // first round: find first point (the one with lowest x)
+  for( int iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y);
+
+    if( x<x0 ) {
+      x0 = x;
+      y0 = y;
+    }
+
+  } // for points
+
+  TGraphErrors* graph_norm = new TGraphErrors(0);
+  graph_norm->SetName( Form("%s_norm", graph->GetName()) );
+
+  // second round: create normalized graph
+  for( int iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y);
+
+    graph_norm->SetPoint( graph_norm->GetN(), x, y/y0 );
+
+  } // for points
+
+  return graph_norm;
 
 }
