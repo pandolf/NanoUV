61eb1c7504f07fff94800a81a188e8a159865c7b
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2020-02-25 16:27:46 +0100

    compatible with new arc/argv scheme

--- ElectronGunAnalysis/analysis/run_drawAPDGunScan.sh
+++ ElectronGunAnalysis/analysis/run_drawAPDGunScan.sh
@@ -1,9 +1,9 @@
-./drawAPDGunScan 900 350;
-./drawAPDGunScan 900 360;
-./drawAPDGunScan 900 370;
-./drawAPDGunScan 900 380;
+./drawAPDGunScan 2020_01 900 350;
+./drawAPDGunScan 2020_01 900 360;
+./drawAPDGunScan 2020_01 900 370;
+./drawAPDGunScan 2020_01 900 380;
 
-#./drawAPDGunScan 500 350;
-#./drawAPDGunScan 500 360;
-#./drawAPDGunScan 500 370;
-#./drawAPDGunScan 500 380;
+#./drawAPDGunScan 2020_01 500 350;
+#./drawAPDGunScan 2020_01 500 360;
+#./drawAPDGunScan 2020_01 500 370;
+#./drawAPDGunScan 2020_01 500 380;
