d83ce8aaf31293bdbd210b8822b4c56be93dc0db
pandolf <francesco.pandolfi@cern.ch>
2020-06-24 10:23:54 +0200

    now subtracting min

--- ElectronGunAnalysis/analysis/checkCurrentSyst.cpp
+++ ElectronGunAnalysis/analysis/checkCurrentSyst.cpp
@@ -17,31 +17,39 @@
 
 
 
+float getSyst( float iNominal, GunScan* scan, const std::string& method );
+TGraphErrors* subtractMin( TGraphErrors* graph );
+
+
 int main( int argc, char* argv[] ) {
 
 
   float APDhv;
+  float gunEnergy;
 
   if( argc==1 ) {
 
-    std::cout << "USAGE: ./checkCurrentSyst [APDhv]" << std::endl;
+    std::cout << "USAGE: ./checkCurrentSyst [gunEnergy] [APDhv=350 V]" << std::endl;
     exit(1);
 
   } else {
 
-    APDhv = atof(argv[1]);
+    gunEnergy = atof(argv[1]);
+    if( argc>2 )
+      APDhv = atof(argv[2]);
+    else
+      APDhv = 350.;
 
   }
 
-  float gunEnergy = 900.;
 
   NanoUVCommon::setStyle();
 
-  std::vector< GunScan* > scans  = GunScan::loadScans( "data/2020_02/scans.txt", 900, APDhv );
+  std::vector< GunScan* > scans  = GunScan::loadScans( "data/2020_02/scans.txt", gunEnergy, APDhv );
 
   TH1D* h1_syst = new TH1D( "syst", "", 50, -0.8, 0.8 );
 
-  TGraphErrors* gr_syst = new TGraphErrors(0);
+  TGraphErrors* gr_syst   = new TGraphErrors(0);
 
 
   for( unsigned iScan=0; iScan<scans.size(); iScan++ ) {
@@ -50,31 +58,26 @@ int main( int argc, char* argv[] ) {
 
     float iapd_nominal = scans[iScan]->getCurrentFromScan();
 
-    scans[iScan]->set_currentMethod("integral");
-    float iapd_syst = scans[iScan]->getCurrentFromScan();
+    float syst = getSyst( iapd_nominal, scans[iScan], "max" );
 
-    std::cout << std::endl;
-    std::cout << "---- Gun current: " << scans[iScan]->gunCurrent() << std::endl;
-    std::cout << "Nominal I(apd): " << iapd_nominal << std::endl;
-    std::cout << "Syst   I(apd): " << iapd_syst << std::endl;
+    //h1_syst ->Fill( syst );
 
-    float syst = fabs(iapd_nominal-iapd_syst)/iapd_nominal;
-    h1_syst ->Fill( syst );
-
-    gr_syst->SetPoint( gr_syst->GetN(), scans[iScan]->gunCurrent(), syst );
+    gr_syst  ->SetPoint( gr_syst  ->GetN(), scans[iScan]->gunCurrent(), syst   );
 
   } // for scans
 
 
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
+  c1->SetLogx();
   c1->cd();
 
-  TH2D* h2_axes = new TH2D( "axes", "", 10, 0., 20., 10, 0., 1. );
+  TH2D* h2_axes = new TH2D( "axes", "", 10, 0.05, 80., 10, 0., 0.4 );
   h2_axes->SetXTitle( "I_{gun} [pA]" );
-  h2_axes->SetYTitle( "I_{apd} [pA]" );
+  h2_axes->SetYTitle( "Relative Uncertainty" );
   h2_axes->Draw();
 
-  TPaveText* label_settings = new TPaveText( 0.23, 0.65, 0.5, 0.77, "brNDC" );
+  TPaveText* label_settings = new TPaveText( 0.7, 0.65, 0.9, 0.77, "brNDC" );
+  //TPaveText* label_settings = new TPaveText( 0.23, 0.65, 0.5, 0.77, "brNDC" );
   label_settings->SetTextSize( 0.035 );
   label_settings->SetFillColor(0);
   label_settings->AddText( Form("E_{e} = %.0f eV", gunEnergy) );
@@ -83,12 +86,20 @@ int main( int argc, char* argv[] ) {
   label_settings->Draw("same");
 
   gr_syst->SetMarkerStyle(20);
-  gr_syst->SetMarkerSize(1.3);
+  gr_syst->SetMarkerSize(1.6);
   gr_syst->SetMarkerColor(kGray+3);
   gr_syst->SetLineColor(kGray+3);
   gr_syst->Draw("Psame");
 
-  NanoUVCommon::addNanoUVLabel( c1, 2 );
+  TGraphErrors* gr_syst_subtractMin = subtractMin( gr_syst );
+  gr_syst_subtractMin->SetMarkerStyle(24);
+  gr_syst_subtractMin->SetMarkerSize(1.6);
+  gr_syst_subtractMin->SetMarkerColor(kGray+3);
+  gr_syst_subtractMin->SetLineColor(kGray+3);
+  gr_syst_subtractMin->Draw("Psame");
+
+
+  NanoUVCommon::addNanoUVLabel( c1, 1 );
 
   label_settings->Draw("same");
 
@@ -103,3 +114,48 @@ int main( int argc, char* argv[] ) {
   return 0;
 
 }    
+
+
+float getSyst( float iNominal, GunScan* scan, const std::string& method ) {
+
+  scan->set_currentMethod(method);
+  float iapd_syst = scan->getCurrentFromScan();
+
+  //std::cout << std::endl;
+  //std::cout << "---- Gun current: " << scans[iScan]->gunCurrent() << std::endl;
+  //std::cout << "Nominal I(apd): " << iapd_nominal << std::endl;
+  //std::cout << "Syst   I(apd): " << iapd_syst << std::endl;
+
+  return fabs(iNominal-iapd_syst)/iNominal;
+
+}
+
+
+
+TGraphErrors* subtractMin( TGraphErrors* graph ) {
+
+  TGraphErrors* newGraph = new TGraphErrors(0);
+
+  float yMin = 10.;
+
+  for( unsigned iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y );
+
+    if( y<yMin ) yMin = y;
+
+  }
+
+  for( unsigned iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y );
+
+    newGraph->SetPoint( newGraph->GetN(), x, y-yMin );
+
+  }
+
+  return newGraph;
+
+}
