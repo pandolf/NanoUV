3f8b68b87ab25dc8051723e6c9d86c6371d193c6
pandolf <francesco.pandolfi@cern.ch>
2020-06-24 17:01:40 +0200

    first commit

---
+++ ElectronGunAnalysis/analysis/copyPlotsToPaper.sh
@@ -0,0 +1,9 @@
+cp ~/FISICA/NanoUV/ElectronGunAnalysis/analysis/plots/APDscans/2020_02/500eV/350V/scan_27feb20_03_M_.dat.pdf ~/FISICA/NanoUV/papers/APD\ characterization/figures/scanAPD.pdf
+cp ~/FISICA/NanoUV/ElectronGunAnalysis/analysis/plots/APDscans/2020_02/500eV/350V/scanCorr_27feb20_03_M_.dat.pdf ~/FISICA/NanoUV/papers/APD\ characterization/figures/scanAPD_corr.pdf
+
+cp ~/FISICA/NanoUV/ElectronGunAnalysis/analysis/plots/APDscans/baselines/900eV/350V/baseline_28feb20_05_M_.dat.pdf ~/FISICA/NanoUV/papers/APD\ characterization/figures/apdSyst.pdf
+
+cp ~/FISICA/NanoUV/ElectronGunAnalysis/analysis/plots/APDscans/2020_02/500eV/350V/iapd_vs_igun_log.pdf ~/FISICA/NanoUV/papers/APD\ characterization/figures/iapd_vs_igun_E500_V350.pdf
+cp ~/FISICA/NanoUV/ElectronGunAnalysis/analysis/plots/APDscans/2020_02/500eV/380V/iapd_vs_igun_log.pdf ~/FISICA/NanoUV/papers/APD\ characterization/figures/iapd_vs_igun_E500_V380.pdf
+cp ~/FISICA/NanoUV/ElectronGunAnalysis/analysis/plots/APDscans/2020_02/900eV/350V/iapd_vs_igun_log.pdf ~/FISICA/NanoUV/papers/APD\ characterization/figures/iapd_vs_igun_E900_V350.pdf
+cp ~/FISICA/NanoUV/ElectronGunAnalysis/analysis/plots/APDscans/2020_02/900eV/380V/iapd_vs_igun_log.pdf ~/FISICA/NanoUV/papers/APD\ characterization/figures/iapd_vs_igun_E900_V380.pdf
