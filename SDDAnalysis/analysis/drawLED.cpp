dbbbeb0b820eb9be91a0d9aa8e8366ac80843556
pandolf <francesco.pandolfi@cern.ch>
2020-07-31 16:37:26 +0200

    first commit of drawLED

---
+++ SDDAnalysis/analysis/drawLED.cpp
@@ -0,0 +1,125 @@
+#include "NanoUVCommon.h"
+
+#include <iostream>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include "TH1D.h"
+#include "TH2D.h"
+
+
+
+
+int main( int argc, char* argv[] ) {
+
+  NanoUVCommon::setStyle();
+
+  if( argc<2 ) {
+
+    std::cout << "USAGE: ./drawLED [ledType=SP5605_SN030, ]" << std::endl;
+    exit(1);
+
+  }
+
+  std::string ledType(argv[1]);
+
+  std::vector< std::pair<float, float> > ag; 
+
+  if( ledType=="SP5605" ) ledType = "SP5605_SN030";
+
+  if( ledType=="SP5605_SN030" ) {
+
+    ag.push_back( std::pair<float,float>(10., 4.) );
+    ag.push_back( std::pair<float,float>( 9., 4.) );
+    ag.push_back( std::pair<float,float>( 8., 4.) );
+    ag.push_back( std::pair<float,float>( 7., 4.) );
+    ag.push_back( std::pair<float,float>( 6., 4.) );
+    //ag.push_back( std::pair<float,float>( 6., 12.) );
+    ag.push_back( std::pair<float,float>( 5.,12.) );
+    ag.push_back( std::pair<float,float>( 4.,40.) );
+
+  }
+
+
+
+  for( unsigned i=0; i<ag.size(); ++i ) {
+
+    float a = ag[i].first;
+    float g = ag[i].second;
+
+    TChain* chain = new TChain("tree");
+
+    //TFile* file = TFile::Open( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020.root", ledType.c_str(), a, g) );
+    chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020.root/tree"  , ledType.c_str(), a, g) );
+    chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020_2.root/tree", ledType.c_str(), a, g) );
+
+
+
+//  TTree* tree = (TTree*)file->Get("tree");
+
+//  if( tree!=0 ) {
+
+//    chain->Add( Form("%s/tree", file->GetName()) );
+//    std::cout << "-> Added: " << file->GetName() << "/tree" << std::endl;
+
+//    TFile* file_2 = TFile::Open( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020_2.root", ledType.c_str(), a, g) );
+
+//    if( !file_2->IsZombie() ) {
+
+//      TTree* tree_2 = (TTree*)file_2->Get("tree");
+
+//      if( tree_2!=0 ) {
+
+//        chain->Add( Form("%s/tree", file_2->GetName()) );
+//        std::cout << "-> Added: " << file_2->GetName() << "/tree" << std::endl;
+
+//        TFile* file_3 = TFile::Open( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020_3.root", ledType.c_str(), a, g) );
+//  
+//        if( !file_3->IsZombie() ) {
+//  
+//          TTree* tree_3 = (TTree*)file_3->Get("tree");
+//  
+//          if( tree_3!=0 ) {
+//  
+//            chain->Add( Form("%s/tree", file_3->GetName()) );
+//            std::cout << "-> Added: " << file_3->GetName() << "/tree" << std::endl;
+
+//          } // if tree_3
+
+//        } // if file_3
+
+//      } // if tree_2
+
+//    } // if file_2
+
+//  } // if tree
+
+    
+    TH1D* h1_vamp = new TH1D( Form("vamp_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "", 1000, 0., 2.5 );
+    chain->Project( h1_vamp->GetName(), "vamp", "vamp>0." );
+
+    TF1* f1_gaus0 = new TF1( Form("gaus0_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "gaus", h1_vamp->GetMean()-0.5*h1_vamp->GetRMS(), h1_vamp->GetMean()+h1_vamp->GetRMS() );
+    h1_vamp->Fit( f1_gaus0, "R0" );
+
+    TF1* f1_gaus = new TF1( Form("gaus_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "gaus", f1_gaus0->GetParameter(1)-1.5*f1_gaus0->GetParameter(2), f1_gaus0->GetParameter(1)+2.5*f1_gaus0->GetParameter(2) );
+    h1_vamp->Fit( f1_gaus, "R" );
+
+    TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
+    c1->cd();
+ 
+    TH2D* h2_axes = new TH2D( Form("axes_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "", 10, f1_gaus0->GetParameter(1)*0.9, f1_gaus0->GetParameter(1)*1.05, 10, 0., 1000. );
+    h2_axes->Draw();
+    h1_vamp->Draw("same");
+
+    c1->SaveAs( "test.pdf" );
+
+    exit(1);
+
+  } // for ag
+
+  return 0;
+
+}
