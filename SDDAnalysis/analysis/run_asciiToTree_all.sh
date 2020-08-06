7b4e691310748323d40d5aef13bac0955c2cd0c3
pandolf <francesco.pandolfi@cern.ch>
2020-08-06 15:58:20 +0200

    first commit

---
+++ SDDAnalysis/analysis/run_asciiToTree_all.sh
@@ -0,0 +1,26 @@
+./asciiFileToTree data/LED_SP5601_SN134/Run_A10_G12_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN134/Run_A7_G120_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN134/Run_A8_G120_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN134/Run_A9_G40_Data_7_23_2020_Ascii.dat
+
+./asciiFileToTree data/LED_SP5601_SN243/Run_A10_G12_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN243/Run_A7_G120_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN243/Run_A8_G120_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN243/Run_A8_G40_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN243/Run_A9_G12_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5601_SN243/Run_A9_G40_Data_7_23_2020_Ascii.dat
+
+./asciiFileToTree data/LED_SP5605_SN030/Run_A10_G4_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A4_G40_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A5_G12_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A6_G12_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A6_G12_Data_7_23_2020_2_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A6_G12_Data_7_23_2020_3_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A6_G4_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A6_G4_Data_7_23_2020_2_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A7_G4_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A7_G4_Data_7_23_2020_2_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A8_G4_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A8_G4_Data_7_23_2020_2_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A9_G4_Data_7_23_2020_Ascii.dat
+./asciiFileToTree data/LED_SP5605_SN030/Run_A9_G4_Data_7_23_2020_2_Ascii.dat
