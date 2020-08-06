165f1ac2637c015c40ecb264f917373892a196b2
pandolf <francesco.pandolfi@cern.ch>
2020-08-06 16:18:41 +0200

    better plotting

--- SDDAnalysis/analysis/drawLED.cpp
+++ SDDAnalysis/analysis/drawLED.cpp
@@ -26,6 +26,7 @@ int main( int argc, char* argv[] ) {
   }
 
   float adc2kev = 8.12501; // from drawFe55
+  float energy;
 
   std::string ledType(argv[1]);
 
@@ -45,6 +46,8 @@ int main( int argc, char* argv[] ) {
     ag.push_back( std::pair<float,float>( 5.,12.) );
     ag.push_back( std::pair<float,float>( 4.,40.) );
 
+    energy = 0.005;
+
   } else if( ledType=="SP5601_SN134" ) {
 
     ag.push_back( std::pair<float,float>(10., 12.) );
@@ -52,6 +55,8 @@ int main( int argc, char* argv[] ) {
     ag.push_back( std::pair<float,float>( 8.,120.) );
     ag.push_back( std::pair<float,float>( 7.,120.) );
  
+    energy = 0.0031;
+
   } else if( ledType=="SP5601_SN243" ) {
 
     ag.push_back( std::pair<float,float>(10.,  12.) );
@@ -60,6 +65,8 @@ int main( int argc, char* argv[] ) {
     ag.push_back( std::pair<float,float>( 8.,  40.) );
     //ag.push_back( std::pair<float,float>( 8., 120.) );
     ag.push_back( std::pair<float,float>( 7., 120.) );
+
+    energy = 0.0031;
 
   } else {
 
@@ -71,7 +78,8 @@ int main( int argc, char* argv[] ) {
   std::string outdir( Form("plots/%s", ledType.c_str()) );
   system( Form("mkdir -p %s", outdir.c_str()) );
 
-  TGraphErrors* graph = new TGraphErrors(0);
+  TGraphErrors* gr_kev_vs_ampl      = new TGraphErrors(0);
+  TGraphErrors* gr_nPhotons_vs_ampl = new TGraphErrors(0);
 
 
   for( unsigned i=0; i<ag.size(); ++i ) {
@@ -82,9 +90,14 @@ int main( int argc, char* argv[] ) {
     TChain* chain = new TChain("tree");
 
     //TFile* file = TFile::Open( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020.root", ledType.c_str(), a, g) );
-    std::cout <<  Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020.root/tree", ledType.c_str(), a, g) << std::endl;
-    chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020.root/tree"  , ledType.c_str(), a, g) );
-    chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020_2.root/tree", ledType.c_str(), a, g) );
+
+    //std::cout <<  Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020.root/tree", ledType.c_str(), a, g) << std::endl;
+    //chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020.root/tree"  , ledType.c_str(), a, g) );
+    //chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Measurements_Only_7_23_2020_2.root/tree", ledType.c_str(), a, g) );
+
+    std::cout <<  Form("data/LED_%s/Run_A%.0f_G%.0f_Data_7_23_2020_Ascii.root/tree", ledType.c_str(), a, g) << std::endl;
+    chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Data_7_23_2020_Ascii.root/tree"  , ledType.c_str(), a, g) );
+    chain->Add( Form("data/LED_%s/Run_A%.0f_G%.0f_Data_7_23_2020_2_Ascii.root/tree", ledType.c_str(), a, g) );
 
 
 
@@ -128,8 +141,8 @@ int main( int argc, char* argv[] ) {
 //  } // if tree
 
     
-    TH1D* h1_vamp = new TH1D( Form("vamp_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "", 1000, 0., 2.5 );
-    chain->Project( h1_vamp->GetName(), "vamp", "vamp>0." );
+    TH1D* h1_vamp = new TH1D( Form("vamp_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "", 2000, 0., 2.5 );
+    chain->Project( h1_vamp->GetName(), "vamp", "vamp>0. && delta>-0.05" );
 
     TF1* f1_gaus0 = new TF1( Form("gaus0_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "gaus", h1_vamp->GetMean()-0.5*h1_vamp->GetRMS(), h1_vamp->GetMean()+h1_vamp->GetRMS() );
     h1_vamp->Fit( f1_gaus0, "R0" );
@@ -140,7 +153,7 @@ int main( int argc, char* argv[] ) {
     TCanvas* c1 = new TCanvas( Form("c1_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "", 600, 600 );
     c1->cd();
  
-    TH2D* h2_axes = new TH2D( Form("axes_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "", 10, f1_gaus0->GetParameter(1)*0.5, f1_gaus0->GetParameter(1)*1.5, 10, 0., 1.2*h1_vamp->GetMaximum() );
+    TH2D* h2_axes = new TH2D( Form("axes_%s_a%.0f_g%.0f", ledType.c_str(), a, g), "", 10, f1_gaus->GetParameter(1)-7.*f1_gaus->GetParameter(2), f1_gaus->GetParameter(1)+7.*f1_gaus->GetParameter(2), 10, 0., 1.2*h1_vamp->GetMaximum() );
     h2_axes->Draw();
     h1_vamp->Draw("same");
 
@@ -148,8 +161,11 @@ int main( int argc, char* argv[] ) {
 
     float k = (adc2kev)*4./g; // Fe55 was taken at gain = 4 (?)
     //float k = (adc2kev/energy)*4./g; // Fe55 was taken at gain = 4 (?)
-    graph->SetPoint     ( i,  a, f1_gaus->GetParameter(1)*k );
-    graph->SetPointError( i, 0., f1_gaus->GetParameter(2)*k );
+    gr_kev_vs_ampl->SetPoint     ( i,  a, f1_gaus->GetParameter(1)*k );
+    gr_kev_vs_ampl->SetPointError( i, 0., f1_gaus->GetParameter(2)*k );
+
+    gr_nPhotons_vs_ampl->SetPoint     ( i,  a, f1_gaus->GetParameter(1)*k/energy );
+    gr_nPhotons_vs_ampl->SetPointError( i, 0., f1_gaus->GetParameter(2)*k/energy );
 
   } // for ag
 
@@ -158,19 +174,35 @@ int main( int argc, char* argv[] ) {
   TCanvas* c2 = new TCanvas( "c2", "", 600, 600 );
   c2->cd();
 
-  TH2D* h2_axes_2 = new TH2D( "axes", "", 10, 3.5, 10.5, 10, 0., 19. );
+  TH2D* h2_axes_2 = new TH2D( "axes_2", "", 10, 3.5, 10.5, 10, 0., 19. );
   h2_axes_2->SetYTitle( "SDD amplitude [keV]" );
   h2_axes_2->SetXTitle( "LED amplitude" );
   h2_axes_2->Draw();
 
-  graph->SetMarkerStyle(20);
-  graph->SetMarkerColor(kGray+3);
-  graph->SetLineColor(kGray+3);
-  graph->SetMarkerSize(1.5);
+  gr_kev_vs_ampl->SetMarkerStyle(20);
+  gr_kev_vs_ampl->SetMarkerColor(kGray+3);
+  gr_kev_vs_ampl->SetLineColor(kGray+3);
+  gr_kev_vs_ampl->SetMarkerSize(1.5);
 
-  graph->Draw("P same");
+  gr_kev_vs_ampl->Draw("P same");
 
-  c2->SaveAs( Form("%s/s_vs_ampl.pdf", outdir.c_str()) );
+  c2->SaveAs( Form("%s/kev_vs_ampl.pdf", outdir.c_str()) );
+
+  c2->Clear();
+
+  TH2D* h2_axes_3 = new TH2D( "axes_3", "", 10, 3.5, 10.5, 10, 0., 80000. );
+  h2_axes_3->SetYTitle( "Number of photons" );
+  h2_axes_3->SetXTitle( "LED amplitude" );
+  h2_axes_3->Draw();
+
+  gr_nPhotons_vs_ampl->SetMarkerStyle(20);
+  gr_nPhotons_vs_ampl->SetMarkerColor(kGray+3);
+  gr_nPhotons_vs_ampl->SetLineColor(kGray+3);
+  gr_nPhotons_vs_ampl->SetMarkerSize(1.5);
+
+  gr_nPhotons_vs_ampl->Draw("P same");
+
+  c2->SaveAs( Form("%s/nPhotons_vs_ampl.pdf", outdir.c_str()) );
 
   return 0;
 
