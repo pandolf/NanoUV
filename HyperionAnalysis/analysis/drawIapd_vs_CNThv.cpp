db050e27653705a69820114e5e1053bda22619d3
pandolf <francesco.pandolfi@cern.ch>
2020-11-16 12:25:31 +0100

    saving plots

--- HyperionAnalysis/analysis/drawIapd_vs_CNThv.cpp
+++ HyperionAnalysis/analysis/drawIapd_vs_CNThv.cpp
@@ -68,6 +68,10 @@ void drawAll( const std::string& name ) {
   }
 
 
+  std::string outdir(Form("plots/%s", name.c_str()));
+  system( Form("mkdir -p %s", outdir.c_str()) );
+
+
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
   c1->cd();
   //c1->SetLogy();
@@ -89,7 +93,6 @@ void drawAll( const std::string& name ) {
   reds.push_back( kRed+2 );
   reds.push_back( kRed+3 );
 
-  TGraphErrors* gr_deltaV_vs_L = new TGraphErrors(0);
 
   for( unsigned i=0; i<v_hd.size(); ++i ) {
 
@@ -99,29 +102,100 @@ void drawAll( const std::string& name ) {
     graph->Draw("PL same");
     legend->AddEntry( graph, Form("L = %.1f mm", v_hd[i].L()), "P" );
 
-    float err_deltaV = 0.;
-    float deltaV = findVfixedThreshold( graph, 200., err_deltaV );
-
-    std::cout << "L: " << v_hd[i].L() << "   found deltaV: " << deltaV << std::endl;
-
-    if( deltaV<0. ) {
-      gr_deltaV_vs_L->SetPoint     ( i, v_hd[i].L(), deltaV );
-      gr_deltaV_vs_L->SetPointError( i, 0., err_deltaV );
-    }
-
   }
 
   legend->Draw("same");
 
   gPad->RedrawAxis();
 
-  c1->SaveAs( Form("Iapd_vs_CNThv_%s.pdf", name.c_str()) );
+  c1->SaveAs( Form("%s/Iapd_vs_CNThv_%s.pdf", outdir.c_str(), name.c_str()) );
 
 
-  // find E
+  // now look for E
 
-  TF1* f1_line = new TF1( "line", "[0] + [1]*x", 25., 36. );
-  gr_deltaV_vs_L->Fit( f1_line, "R" );
+  std::vector<float> thresholds;
+  thresholds.push_back( 125. );
+  thresholds.push_back( 200. );
+  thresholds.push_back( 300. );
+  thresholds.push_back( 400. );
+  thresholds.push_back( 500. );
+
+  TGraphErrors* gr_d0_vs_thresh = new TGraphErrors(0);
+  TGraphErrors* gr_E_vs_thresh  = new TGraphErrors(0);
+
+  gr_d0_vs_thresh->SetName("d0_vs_thresh");
+  gr_E_vs_thresh ->SetName("E_vs_thresh");
+
+  for( unsigned ithresh=0; ithresh<thresholds.size(); ++ithresh ) {
+
+    std::cout << "-> Starting threshold: " << thresholds[ithresh] << std::endl;
+
+    TGraphErrors* gr_deltaV_vs_L = new TGraphErrors(0);
+    gr_deltaV_vs_L->SetName( Form( "deltaV_vs_L_th%.0f", thresholds[ithresh]) );
+
+    for( unsigned i=0; i<v_hd.size(); ++i ) {
+
+      float err_deltaV = 0.;
+      float deltaV = findVfixedThreshold( v_hd[i].getGraphFromColumns( Form("graph_%d", i), 1, 2, 1 ), thresholds[ithresh], err_deltaV );
+
+      std::cout << "L: " << v_hd[i].L() << "   found deltaV: " << deltaV << std::endl;
+
+      if( deltaV<0. ) {
+        int iPoint = gr_deltaV_vs_L->GetN();
+        gr_deltaV_vs_L->SetPoint     ( iPoint, -v_hd[i].L(), -deltaV );
+        gr_deltaV_vs_L->SetPointError( iPoint, 0., err_deltaV );
+      }
+
+    } // for ipoints
+
+    // find E
+
+    TF1* f1_line = new TF1( "line", "[0] + [1]*x" );
+    f1_line->SetLineColor(kGray+3);
+    f1_line->SetLineWidth(2);
+    gr_deltaV_vs_L->SetMarkerStyle(20);
+    gr_deltaV_vs_L->SetMarkerSize(1.5);
+    gr_deltaV_vs_L->SetMarkerColor(46);
+    gr_deltaV_vs_L->Fit( f1_line, "X" );
+
+    TCanvas* c2 = new TCanvas( "c2", "", 600, 600 );
+    c2->cd();
+
+    TH2D* h2_axes2 = new TH2D( "axes", "", 10, -40., -20., 10, 0., 2000. );
+    h2_axes2->SetXTitle( "-L [mm]" );
+    h2_axes2->SetYTitle( "#DeltaV [V]" );
+    h2_axes2->Draw();
+
+    gr_deltaV_vs_L->Draw("Psame");
+
+    c2->SaveAs( Form("%s/deltaV_vs_L_th%.0f.pdf", outdir.c_str(), thresholds[ithresh]) );
+
+    delete c2;
+    delete h2_axes2;
+
+    // L = d0-d
+    float d0     = f1_line->GetParameter(0);
+    float err_d0 = f1_line->GetParError(0);
+    // DeltaV/d = E
+    float E     = f1_line->GetParameter(1);
+    float err_E = f1_line->GetParError(1);
+
+    gr_d0_vs_thresh->SetPoint     ( ithresh, thresholds[ithresh], d0 ); 
+    gr_d0_vs_thresh->SetPointError( ithresh, 0., err_d0 );
+
+    gr_E_vs_thresh->SetPoint      ( ithresh, thresholds[ithresh], E ); 
+    gr_E_vs_thresh->SetPointError ( ithresh, 0., err_E );
+
+  }
+
+  
+TFile* file = new TFile( "test.root", "recreate" );
+file->cd();
+gr_d0_vs_thresh->Write();
+gr_E_vs_thresh->Write();
+file->Close();
+exit(1);
+
 
   delete h2_axes;
   delete c1;
