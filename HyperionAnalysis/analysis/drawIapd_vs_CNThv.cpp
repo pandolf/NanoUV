94b4c1e20e5afc3b0f5d4d36e677d38d90c6bbe7
pandolf <francesco.pandolfi@cern.ch>
2020-11-16 10:24:45 +0100

    added nanofusilli

--- HyperionAnalysis/analysis/drawIapd_vs_CNThv.cpp
+++ HyperionAnalysis/analysis/drawIapd_vs_CNThv.cpp
@@ -9,25 +9,66 @@
 #include "TLegend.h"
 
 
+void drawAll( const std::string& name );
+
 
 int main() {
 
   NanoUVCommon::setStyle();
 
+  drawAll( "CNT50um_fusedITO" );
+  drawAll( "NanoFusilli_boroSiGlass" );
+
+  return 0;
+
+}
+
+
+void drawAll( const std::string& name ) {
+
+  std::cout << std::endl << "-> Starting: " << name << std::endl;
+
   std::vector< HyperionData > v_hd;
-  v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L32_CNT50um_fusedITO.dat"  ) );
-  v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L33_CNT50um_fusedITO.dat"  ) );
-  v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L33p5_CNT50um_fusedITO.dat") );
-  v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L34_CNT50um_fusedITO.dat"  ) );
-  v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L34p5_CNT50um_fusedITO.dat") );
-  v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L35_CNT50um_fusedITO.dat"  ) );
+
+  float xMin = -2100.;
+  float yMax = 1500.;
+
+  if( name=="CNT50um_fusedITO" ) {
+
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L32_CNT50um_fusedITO.dat"  ) );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L33_CNT50um_fusedITO.dat"  ) );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L33p5_CNT50um_fusedITO.dat") );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L34_CNT50um_fusedITO.dat"  ) );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L34p5_CNT50um_fusedITO.dat") );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_29/Iapd_vs_CNThv_L35_CNT50um_fusedITO.dat"  ) );
+
+    xMin = -2100.;
+    yMax = 1200.;
+
+  } else if (name=="NanoFusilli_boroSiGlass" ) {
+
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_30/Iapd_vs_CNThv_L31p5_NanoFusilli_boroSiGlass.dat") );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_30/Iapd_vs_CNThv_L32_NanoFusilli_boroSiGlass.dat"  ) );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_30/Iapd_vs_CNThv_L32p5_NanoFusilli_boroSiGlass.dat") );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_30/Iapd_vs_CNThv_L33_NanoFusilli_boroSiGlass.dat"  ) );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_30/Iapd_vs_CNThv_L33p5_NanoFusilli_boroSiGlass.dat") );
+    v_hd.push_back( HyperionData("data/HyperionAPD/2020_10_30/Iapd_vs_CNThv_L34_NanoFusilli_boroSiGlass.dat"  ) );
+
+    xMin = -700.;
+    yMax = 7000.;
+
+  } else {
+
+    std::cout << "-> Unkown name: " << name << std::endl;
+
+  }
 
 
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
   c1->cd();
   //c1->SetLogy();
 
-  TH2D* h2_axes = new TH2D("axes", "", 10, -2100., 0., 10, 0., 1200.);
+  TH2D* h2_axes = new TH2D("axes", "", 10, xMin, 100., 10, 0., yMax);
   h2_axes->SetXTitle( "#DeltaV [V]" );
   h2_axes->SetYTitle( "I_{apd} [nA]" );
   h2_axes->Draw();
@@ -46,7 +87,7 @@ int main() {
 
   for( unsigned i=0; i<v_hd.size(); ++i ) {
 
-    TGraphErrors* graph = v_hd[i].getGraphFromColumns( Form("graph_%d", i), 1, 2 );
+    TGraphErrors* graph = v_hd[i].getGraphFromColumns( Form("graph_%d", i), 1, 2, 1 );
     graph->SetMarkerColor( reds[i] );
     graph->SetLineColor  ( reds[i] );
     graph->Draw("PL same");
@@ -58,13 +99,10 @@ int main() {
 
   gPad->RedrawAxis();
 
-  c1->SaveAs("Iapd_vs_CNThv.pdf");
+  c1->SaveAs( Form("Iapd_vs_CNThv_%s.pdf", name.c_str()) );
 
-  return 0;
+  delete h2_axes;
+  delete c1;
+  delete legend;
 
 }
-//-rw-r--r--  1 pandolf  staff  222 Nov  5 12:02 Iapd_vs_CNThv_L35_CNT50um_fusedITO_air.dat
-//
-//-rw-r--r--  1 pandolf  staff  239 Nov  5 12:08 Iapd_vs_CNThv_L32_noCNT_air.dat
-//-rw-r--r--  1 pandolf  staff  241 Nov  5 12:06 Iapd_vs_CNThv_L33_noCNT_air.dat
-//-rw-r--r--  1 pandolf  staff  358 Nov  5 12:06 Iapd_vs_CNThv_L34_noCNT_air.dat
