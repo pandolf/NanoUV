0d0a815726f431abbe202c14fdadabed2ad753a8
pandolf <francesco.pandolfi@cern.ch>
2020-11-18 11:56:51 +0100

    cleaned up, now subtracting baseline

--- HyperionAnalysis/analysis/drawIapd_vs_CNThv.cpp
+++ HyperionAnalysis/analysis/drawIapd_vs_CNThv.cpp
@@ -14,6 +14,7 @@
 void drawAll( const std::string& name );
 float findVfixedThreshold( TGraphErrors* graph, float threshold, float& err_deltaV );
 float interpolate( float this_x, float this_y, float next_x, float next_y, float threshold );
+TGraphErrors* subtractBaseline( TGraphErrors* graph );
 
 
 int main() {
@@ -81,6 +82,18 @@ void drawAll( const std::string& name ) {
   h2_axes->SetYTitle( "I_{apd} [nA]" );
   h2_axes->Draw();
 
+
+  TCanvas* c1_subtr = new TCanvas( "c1_subtr", "", 600, 600 );
+  c1_subtr->cd();
+
+  TH2D* h2_axes_subtr = new TH2D("axes_subtr", "", 10, xMin, 100., 10, 0., yMax);
+  h2_axes_subtr->SetXTitle( "#DeltaV [V]" );
+  h2_axes_subtr->SetYTitle( "I_{apd} (baseline subtracted) [nA]" );
+  h2_axes_subtr->Draw();
+
+  h2_axes_subtr->Draw();
+
+
   TLegend* legend = new TLegend( 0.68, 0.6, 0.9, 0.9 );
   legend->SetFillColor(0);
   legend->SetTextSize(0.035);
@@ -93,46 +106,54 @@ void drawAll( const std::string& name ) {
   reds.push_back( kRed+2 );
   reds.push_back( kRed+3 );
 
-  double baseline;
 
   for( unsigned i=0; i<v_hd.size(); ++i ) {
 
     TGraphErrors* graph = v_hd[i].getGraphFromColumns( Form("graph_%d", i), 1, 2, 1 );
+    TGraphErrors* graph_subtr = subtractBaseline( graph );
+
+    c1->cd();
     graph->SetMarkerColor( reds[i] );
     graph->SetLineColor  ( reds[i] );
     graph->Draw("PL same");
-    legend->AddEntry( graph, Form("L = %.1f mm", v_hd[i].L()), "P" );
 
-    // save baseline (will be needed later)
-    if( i==v_hd.size()-1 ) {
-      double dummyx;
-      graph->GetPoint( 0, dummyx, baseline);
-    }
+    c1_subtr->cd();
+    graph_subtr->SetMarkerSize ( 1.5 );
+    graph_subtr->SetMarkerStyle( 20 );
+    graph_subtr->SetMarkerColor( reds[i] );
+    graph_subtr->SetLineColor  ( reds[i] );
+    graph_subtr->Draw("PL same");
+
+    legend->AddEntry( graph, Form("L = %.1f mm", v_hd[i].L()), "P" );
 
   }
 
+  c1->cd();
   legend->Draw("same");
-
   gPad->RedrawAxis();
-
   c1->SaveAs( Form("%s/Iapd_vs_CNThv_%s.pdf", outdir.c_str(), name.c_str()) );
 
+  c1_subtr->cd();
+  legend->Draw("same");
+  gPad->RedrawAxis();
+  c1_subtr->SaveAs( Form("%s/Iapd_vs_CNThv_%s_subtr.pdf", outdir.c_str(), name.c_str()) );
 
-  // now look for E
+
+
 
   std::vector<float> thresholds;
   if( name == "CNT50um_fusedITO" ) {
-    thresholds.push_back( 125. );
+    thresholds.push_back( 25. );
+    thresholds.push_back( 100. );
     thresholds.push_back( 200. );
     thresholds.push_back( 300. );
     thresholds.push_back( 400. );
-    thresholds.push_back( 500. );
   } else if( name == "NanoFusilli_boroSiGlass" ) {
-    thresholds.push_back( 200. );
+    thresholds.push_back( 100. );
     thresholds.push_back( 500. );
     thresholds.push_back( 1000. );
-    thresholds.push_back( 1500. );
     thresholds.push_back( 2000. );
+    thresholds.push_back( 3000. );
   }
 
   TGraphErrors* gr_d0_vs_thresh = new TGraphErrors(0);
@@ -143,6 +164,7 @@ void drawAll( const std::string& name ) {
   gr_E_vs_thresh ->SetName("E_vs_thresh");
   gr_thresh_vs_E ->SetName("thresh_vs_E");
 
+
   for( unsigned ithresh=0; ithresh<thresholds.size(); ++ithresh ) {
 
     std::cout << "-> Starting threshold: " << thresholds[ithresh] << std::endl;
@@ -150,12 +172,16 @@ void drawAll( const std::string& name ) {
     TGraphErrors* gr_deltaV_vs_L = new TGraphErrors(0);
     gr_deltaV_vs_L->SetName( Form( "deltaV_vs_L_th%.0f", thresholds[ithresh]) );
 
+ 
     for( unsigned i=0; i<v_hd.size(); ++i ) {
 
       if( name=="NanoFusilli_boroSiGlass" && v_hd[i].L()<32. ) continue;
 
+      TGraphErrors* graph = v_hd[i].getGraphFromColumns( Form("graph_%d", i), 1, 2, 1 );
+      TGraphErrors* graph_subtr = subtractBaseline( graph );
+
       float err_deltaV = 0.;
-      float deltaV = findVfixedThreshold( v_hd[i].getGraphFromColumns( Form("graph_%d", i), 1, 2, 1 ), thresholds[ithresh], err_deltaV );
+      float deltaV = findVfixedThreshold( graph_subtr, thresholds[ithresh], err_deltaV );
 
       std::cout << "L: " << v_hd[i].L() << "   found deltaV: " << deltaV << std::endl;
 
@@ -216,57 +242,19 @@ void drawAll( const std::string& name ) {
     gr_thresh_vs_E->SetPoint      ( ithresh, E, thresholds[ithresh] );
     gr_thresh_vs_E->SetPointError ( ithresh, err_E, 1. );
 
-  }
+  } // for ithresh
+
 
 
   TCanvas* c3 = new TCanvas( "c3", "", 600, 600 );
   c3->cd();
-
-//  float xMin3 = 0.;
-//  float xMax3 = 1000.;
-//
-//  TH2D* h2_axes3 = new TH2D( "axes3", "", 10, xMin3, xMax3, 10, 0., 600. );
-//  h2_axes3->SetXTitle( "E [V/mm]" );
-//  h2_axes3->SetYTitle( "Threshold [V]" );
-//  h2_axes3->Draw();
-//
-//  TF1* f1_line2 = new TF1("line2", "[0]+[1]*x");
-//  f1_line2->SetLineColor(46);
-//  f1_line2->SetLineWidth(2);
-//  gr_thresh_vs_E->SetMarkerStyle(20);
-//  gr_thresh_vs_E->SetMarkerColor(46);
-//  gr_thresh_vs_E->SetLineColor  (46);
-//  gr_thresh_vs_E->SetMarkerSize(1.5);
-//  gr_thresh_vs_E->Fit( f1_line2, "X" );
-//
-//  gr_thresh_vs_E->Draw("Psame");
-//
-//  TLine* line_baseline = new TLine( xMin3, baseline, xMax3, baseline );
-//  line_baseline->SetLineColor( kGray+2 );
-//  line_baseline->SetLineWidth( 3 );
-//  line_baseline->SetLineStyle( 2 );
-//  line_baseline->Draw("same");
-//
-////float minE = f1_line2->Eval(baseline);
-//
-////TLine* line_minE = new TLine( 0., minE, baseline, minE );
-////line_minE->SetLineColor( 38 );
-////line_minE->SetLineWidth( 3 );
-////line_minE->SetLineStyle( 2 );
-////line_minE->Draw("same");
-//
-//  gPad->RedrawAxis();
-//
-//  c3->SaveAs( Form("%s/thresh_vs_E.pdf", outdir.c_str()) );
- 
-
 
   float xMax3 = thresholds[thresholds.size()-1]*1.2;
   float yMin3 = 0.;
   float yMax3 = 1000.;
 
   TH2D* h2_axes3 = new TH2D( "axes3", "", 10, 0., xMax3, 10, yMin3, yMax3 );
-  h2_axes3->SetXTitle( "I_{apd} Threshold [nA]" );
+  h2_axes3->SetXTitle( "I_{apd} Threshold (baseline subtracted) [nA]" );
   h2_axes3->SetYTitle( "E [V/mm]" );
   h2_axes3->Draw();
 
@@ -281,36 +269,19 @@ void drawAll( const std::string& name ) {
 
   gr_E_vs_thresh->Draw("Psame");
 
-  TLine* line_baseline = new TLine( baseline, yMin3, baseline, yMax3 );
-  line_baseline->SetLineColor( kGray+2 );
-  line_baseline->SetLineWidth( 3 );
-  line_baseline->SetLineStyle( 2 );
-  line_baseline->Draw("same");
-
-  float minE = f1_line2->Eval(baseline);
-
-  TLine* line_minE = new TLine( 0., minE, baseline, minE );
-  line_minE->SetLineColor( 29 );
-  line_minE->SetLineWidth( 3 );
-  line_minE->SetLineStyle( 2 );
-  line_minE->Draw("same");
-
-  //TPaveText* label_name = new TPaveText( 0.4, 0.75, 0.9, 0.9, "brNDC" );
-  //label_name->SetFillColor(0);
-  //label_name->SetTextSize(0.035);
-  //label_name->AddText( "ITO(500nm) + Fused Silica(2mm) + CNT(50#mum)");
-  //label_name->Draw("same");
-
   gPad->RedrawAxis();
 
   c3->SaveAs( Form("%s/E_vs_thresh.pdf", outdir.c_str()) );
  
-//TFile* file = new TFile( "test.root", "recreate" );
-//file->cd();
-//gr_d0_vs_thresh->Write();
-//gr_E_vs_thresh->Write();
-//file->Close();
-//exit(1);
+  std::cout << std::endl;
+  std::cout << "========================================" << std::endl;
+  std::cout << std::endl;
+  std::cout << " -> " << name << " emit for E > " << f1_line2->Eval(0.) << " V/mm" << std::endl;
+  std::cout << std::endl;
+  std::cout << "========================================" << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
 
   c3->Clear();
 
@@ -377,5 +348,44 @@ float interpolate( float this_x, float this_y, float next_x, float next_y, float
   float m = (next_y-this_y)/(next_x-this_x);
   float deltaV =  (threshold-this_y)/m + this_x;
   return deltaV;
+
+}
+
+
+
+TGraphErrors* subtractBaseline( TGraphErrors* graph ) {
+
+  TGraphErrors* return_graph = new TGraphErrors(0);
+  return_graph->SetName( Form("%s_subtr", graph->GetName()) );
+
+  float baseline = 0.;
+
+  for( unsigned iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y );
+
+    if( x==0. ) {
+      baseline = y;
+      break;
+    }    
+
+  } // for points
+
+
+  for( unsigned iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y );
+
+    double x_err = graph->GetErrorX(iPoint);
+    double y_err = graph->GetErrorY(iPoint);
+
+    return_graph->SetPoint     ( iPoint, x, y-baseline );
+    return_graph->SetPointError( iPoint, x_err, y_err );
+
+  } // for points
+
+  return return_graph;
 
 }
