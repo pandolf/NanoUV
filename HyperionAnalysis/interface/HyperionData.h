0b011a3df840b0e39f91f1ff83fc5f91841328e2
pandolf <francesco.pandolfi@cern.ch>
2020-11-23 16:37:18 +0100

    added cnt_sample_

--- HyperionAnalysis/interface/HyperionData.h
+++ HyperionAnalysis/interface/HyperionData.h
@@ -15,13 +15,14 @@ class HyperionData {
 
   HyperionData( const std::string& fileName );
 
-  std::string picoamm() const { return picoamm_ ; };
-  std::string led()     const { return led_     ; };
-  float led_freq()      const { return led_freq_; };
-  float p_before()      const { return p_before_; };
-  float p_after()       const { return p_after_ ; };
-  float p()             const { return 0.5*(p_after_+p_before_) ; };
-  float L()             const { return L_; };
+  std::string picoamm()    const { return picoamm_ ; };
+  std::string led()        const { return led_     ; };
+  std::string cnt_sample() const { return cnt_sample_; };
+  float led_freq()         const { return led_freq_; };
+  float p_before()         const { return p_before_; };
+  float p_after()          const { return p_after_ ; };
+  float p()                const { return 0.5*(p_after_+p_before_) ; };
+  float L()                const { return L_; };
 
   TGraphErrors* getGraphFromColumns( const std::string& name, int columnMeans, int columnErrors=-1, int nMeasurements=1000 ) const;
 
@@ -30,6 +31,7 @@ class HyperionData {
 
   std::string picoamm_; // "keithley" or "keysight"
   std::string led_; // "SP6505" (UV) or "SP6501" (visible) or "off"
+  std::string cnt_sample_; // "SP6505" (UV) or "SP6501" (visible) or "off"
   float led_freq_; // in kHz
   float p_before_; // in mbar
   float p_after_;  // in mbar
