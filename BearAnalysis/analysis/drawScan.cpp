b371d2ece4f6b3e72a7782b3f5dab2145f941722
pandolf <francesco.pandolfi@cern.ch>
2021-05-22 13:56:05 +0200

    taking scanNumber from comand line

--- BearAnalysis/analysis/drawScan.cpp
+++ BearAnalysis/analysis/drawScan.cpp
@@ -1,11 +1,19 @@
 #include "../interface/ScanFileReader.h"
 
+#include <iostream>
 
 
+int main( int argc, char* argv[] ) {
 
-int main() {
 
-  ScanFileReader sfr(11);
+  if( argc<2 ) {
+    std::cout << "USAGE: ./drawScan [scanNumber]" << std::endl;
+    exit(1);
+  }
+
+  int scanNumber( atoi(argv[1]) );
+
+  ScanFileReader sfr(scanNumber);
 
   return 0;
 
