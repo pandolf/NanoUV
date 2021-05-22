e1a913767dc53bda1c0f645da5f8edba472c4c7c
pandolf <francesco.pandolfi@cern.ch>
2021-05-22 14:30:37 +0200

    finally reads file and fills graphs correctly

--- BearAnalysis/src/ScanFileReader.cc
+++ BearAnalysis/src/ScanFileReader.cc
@@ -4,19 +4,21 @@
 #include <fstream>
 #include <iostream>
 
+#include "TFile.h"
+
 
 
 ScanFileReader::ScanFileReader( int scanNumber ) {
 
   std::cout << "[ScanFileReader] Starting scan N." << scanNumber << std::endl;
 
-  gr_mirrorCurrent_ = new TGraphErrors(0);
-  gr_drainCurrent_ = new TGraphErrors(0);
+  gr_mirror_ = new TGraphErrors(0);
+  gr_drain_ = new TGraphErrors(0);
   gr_scan_ = new TGraphErrors(0);
 
-  gr_mirrorCurrent_->SetName( Form("mirrorCurrent_%d", scanNumber) );
-  gr_drainCurrent_->SetName( Form("drainCurrent_%d", scanNumber) );
-  gr_scan_->SetName( Form("scan_%d", scanNumber) );
+  gr_mirror_->SetName( Form("mirror") );
+  gr_drain_ ->SetName( Form("drain") );
+  gr_scan_  ->SetName( Form("scan") );
 
 
   std::string suffix(Form("%d", scanNumber));
@@ -46,6 +48,8 @@ void ScanFileReader::readFile( const std::string& fileName ) {
 
      
   std::string line;
+  int foundLineLines = 0;
+  bool readyToRead = false;
 
   while( getline(ifs,line) ) {
 
@@ -63,17 +67,59 @@ void ScanFileReader::readFile( const std::string& fileName ) {
       words.push_back(word);
     }
 
-    if( words.size() < 2 ) continue;
 
-    if( words[0]=="SCAN" && words[1]=="TYPE:" ) {
+    if( words.size()>1 && words[0]=="SCAN" && words[1]=="TYPE:" ) {
 
       if( thisLine=="SCAN TYPE: Kinetic Energy Electron analyzer " ) scanType_ = "energyScan";
       if( thisLine=="SCAN TYPE: THETAA" ) scanType_ = "thetaScan";
 
       std::cout << "[ScanFileReader::readFile] Recognized scantype: " << scanType_ << std::endl;
 
-    } // if scanType line
+    } else if( thisLine=="________________________________________________________________________________" ) {
+
+      foundLineLines += 1;
+
+      if( foundLineLines==2 ) 
+        readyToRead = true;
+
+
+    } else if( readyToRead ) {
+
+      std::string delimiter = "\t";  // numbers are spaced with tabs
+      size_t pos = 0;
+      std::vector<std::string> numbers;
+      std::string number;
+      while ((pos = thisLine.find(delimiter)) != std::string::npos) {
+        number = thisLine.substr(0, pos);
+        thisLine.erase(0, pos + delimiter.length());
+        numbers.push_back(number);
+      }
+
+
+      if( numbers.size() > 5 ) {
+
+        float x = atof( numbers[3].c_str() );
+        float y_mirror = atof( numbers[5].c_str() );
+        float y_drain  = atof( numbers[4].c_str() );
+        float y_scan   = atof( numbers[6].c_str() );
+
+        gr_mirror_->SetPoint( gr_mirror_->GetN(), x, y_mirror );
+        gr_drain_ ->SetPoint( gr_drain_ ->GetN(), x, y_drain  );
+        gr_scan_  ->SetPoint( gr_scan_  ->GetN(), x, y_scan   );
+
+      } // if numbers size
+
+    } // if readyToRead
 
   } // while getLine
 
+  TFile* file = TFile::Open( "test.root", "recreate" );
+  file->cd();
+  gr_mirror_->Write();
+  gr_drain_ ->Write();
+  gr_scan_  ->Write();
+  file->Close();
+  
+
 }
+
