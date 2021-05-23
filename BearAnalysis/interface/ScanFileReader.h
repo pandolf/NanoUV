8148a3e24d057a70647b3595b8b957500920ba14
pandolf <francesco.pandolfi@cern.ch>
2021-05-23 12:27:36 +0200

    removed, now all in BearScan

--- BearAnalysis/interface/ScanFileReader.h
+++ BearAnalysis/interface/ScanFileReader.h
@@ -15,25 +15,11 @@ class ScanFileReader {
 
   ScanFileReader( int scanNumber, int firstRegion=-1, int lastRegion=-1 );
 
-  void readSingleScan();
-  void readRegionsScan();
-
-  void readFile( const std::string& fileName );
-  void readFile( std::ifstream& ifs );
-
-  void drawGraphs() const;
-  void drawGraph( TGraphErrors* graph, const std::string& name, const std::string& xTitle, const std::string& yTitle ) const;
-
-  void findPlotRanges( TGraph* graph, float& xMin, float& xMax, float& yMin, float& yMax ) const;
 
 
  private:
 
   Scan* scan_;
-
-  TGraphErrors* gr_mirror_;
-  TGraphErrors* gr_drain_;
-  TGraphErrors* gr_scan_;
 
 
 };
